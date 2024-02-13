3eea5ad13362bc5e9bafd127ce33f5722acdf7db
mzaiduu44 <mzaiduu44@gmail.com>
2024-02-13 13:08:54 +0500

    Basic Math Operation

---
+++ app.js
@@ -0,0 +1,51 @@
+// // Alert Syntax
+// alert("Hello World")
+// // To Popup any thing with the help of variable
+// var a;
+// a = 1683;
+// alert( a ); /*This will PopUp this 1683*/
+
+// // Simplifying Multiple Variables
+// // let r = 100
+// //  x = 5;
+// //  console.log ( r * x);
+
+// let number1 = 20;
+// let number2 = 10;
+// let sum = number1 + number2
+// number1 = 30;
+// console.log(sum)
+// sum = 100
+// console.log(sum)
+
+// let digit1 = 5;
+// let digit2 = 10;
+// digit1 = 60
+// digit2 = 20
+// let product = digit1 * digit2
+// console.log(product)
+// product = 120;
+// console.log(product * product)
+
+// let value1 = 40;
+// let value2 = 20;
+// let remainder = value1 / value2
+// console.log(remainder);
+
+// create two variable a and b assign value hand to hand and then apply all four math operation(+,-,*,/) adn assign to sum,sub,mul.div
+
+let a = 20;
+let b = 30;
+let sum,sub,mul,div;
+// Addition
+sum = a + b;
+console.log(sum)
+// Subtraction
+sub = a - b;
+console.log(sub)
+// Multiplication
+mul = a * b;
+console.log(mul)
+// Division
+div = a / b;
+console.log(div)
